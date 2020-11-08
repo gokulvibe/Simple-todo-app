d69802bb7b7af935a5f4e435fa27fb0a4947acb2
Gokul Raam <gokulraamofficial@gmail.com>
2020-11-08 17:21:40 +0530

    Created todolist model and made migrations

--- main/models.py
+++ main/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class TodoList(models.Model):
+    taskname = models.CharField(max_length = 100)
+    done = models.BooleanField(default=False)
