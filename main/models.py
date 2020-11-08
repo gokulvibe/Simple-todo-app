60c38ebe693619da5db09e3aab5f31db0a38cc58
Gokul Raam <gokulraamofficial@gmail.com>
2020-11-08 18:01:41 +0530

    Added user foreign key to todolist model

--- main/models.py
+++ main/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class TodoList(models.Model):
+    owner = models.ForeignKey(User,verbose_name = 'User', related_name = 'TodoLists',on_delete=models.CASCADE)
     taskname = models.CharField(max_length = 100)
     done = models.BooleanField(default=False)
