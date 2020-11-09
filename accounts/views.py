f31ddb38e1633546a1579020123f5ee429e43eab
Gokul Raam <gokulraamofficial@gmail.com>
2020-11-09 21:34:32 +0530

    Added login, register functionality and displayed data from DB in homepage

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
 
 # Create your views here.
 def login(request):
-    pass
-
-def logout(request):
-    pass
+    if request.method=="POST":
+        username=request.POST["username"]
+        password=request.POST["password"]
+        user=auth.authenticate(username=username,password=password)
+        
+        if user is not None:
+            auth.login(request,user)
+            return redirect("/")
+        else:
+            messages.info(request,"Invalid Credentials")
+            return redirect('login')
+    else:
+        return render(request,'accounts/login.html')
 
 def register(request):
-    pass
+    if request.method=='POST':
+        first_name=request.POST["first_name"]
+        last_name=request.POST["last_name"]
+        username=request.POST["username"]
+        password1=request.POST["password1"]
+        password2=request.POST["password2"]
+        email=request.POST["email"]
+        
+        if password1==password2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request,'Username Taken')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request,'E=mail taken')
+                return redirect('register')
+            else:
+                user=User.objects.create_user(username=username,password=password1,email=email,first_name=first_name,last_name=last_name)
+                user.save()
+                print("User Created!")
+                return redirect('login')
+        else:
+            messages.info(request,'Passwords do not match')
+            return redirect('register')
+        
+    else:
+        return render(request,'accounts/register.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
--- main/views.py
+++ main/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import TodoList
+from accounts.views import *
 # Create your views here.
+
+
 def home(request):
-    return render(request,"main/index.html",{})
+    todos = TodoList.objects.all()
+    
+    return render(request,'main/index.html',{'todos':todos})
